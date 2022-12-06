9d3be01fdfdec6463505e6733e3c0633a4724e88
Sathish <sathishkolaks143@gmail.com>
2022-12-06 15:58:52 +0530

    changed http url to https

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ function loadMovieDetails(){
         movie.addEventListener('click', async () => {
             searchList.classList.add('hide-search-list');
             movieSearchBox.value = "";
-            const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=fb15ca98`);
+            const result = await fetch(`https://www.omdbapi.com/?i=${movie.dataset.id}&apikey=fb15ca98`);
             const movieDetails = await result.json();
             displayMovieDetails(movieDetails);
         });
